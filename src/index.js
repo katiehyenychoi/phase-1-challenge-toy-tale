a03c4de46325ed7754f3a6624f9d03ff2b0d6ba8
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-14 10:24:15 -0400

    Add Toy Info to the Card Done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollectionDiv = document.querySelector("#toy-collection");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +14,34 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(toyJson => {
+      // console.log(toyJson)
+      renderToys(toyJson)
+    })
+
+
+  function renderToys(toyArr) {
+    toyArr.forEach(toy => {
+      let toyDiv = document.createElement("div")
+      toyDiv.className = "card"
+      let toyH2 = document.createElement("h2")
+      toyH2.innerText = toy.name
+      let toyImage = document.createElement("img")
+      toyImage.className = "toy-avatar"
+      toyImage.src = toy.image
+      let toyP = document.createElement("p")
+      toyP.innerText = `${toy.likes} Likes`
+      let toyBtn = document.createElement("button")
+      toyBtn.className = "like-btn"
+      toyBtn.id = toy.id
+      toyBtn.innerText = "Like <3"
+
+      toyDiv.append(toyH2, toyImage, toyP, toyBtn)
+      toyCollectionDiv.append(toyDiv)
+    })
+  }
+
 });
